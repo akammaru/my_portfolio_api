3fa87b93291d573335817b73036b07fd70ed96cd
vagrant <vagrant@homestead>
2018-09-12 14:24:36 +0000

    initial setup for multi language

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,8 @@ use Illuminate\Http\Request;
 |
 */
 
+Route::get('/translations', 'TranslationsController@getAllTranslations');
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
