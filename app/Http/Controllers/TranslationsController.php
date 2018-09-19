4ee75c1dd90996a11e6bdee863f79d8c8573f2e5
Bas Horselenberg <bashorselenberg@gmail.com>
2018-09-19 09:58:31 +0200

    Revert "more bug fixes. Language now correctly fetched"

--- app/Http/Controllers/TranslationsController.php
+++ app/Http/Controllers/TranslationsController.php
@@ -9,6 +9,6 @@
     {
         public function getAllTranslations(Request $request)
         {
-            return Translation::where('language', $request->language)->get();
+            return Translation::where('language', $request->language)->all();
         }
     }
