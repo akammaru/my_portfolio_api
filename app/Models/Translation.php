67953026b09ddc7ce1aaf91d5363d9df16fad924
Bas Horselenberg <bashorselenberg@gmail.com>
2018-10-04 08:32:09 +0200

    text translations now ready and working

--- app/Models/Translation.php
+++ app/Models/Translation.php
@@ -1,15 +1,20 @@
 <?php
 
-namespace App\Models;
+namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Translation extends Model
 {
+    use SoftDeletes;
+
+    public $timestamps = true;
+
     protected $fillable = [
         "label",
         "category",
         "translation",
-        "language",
+        "language"
     ];
 }
